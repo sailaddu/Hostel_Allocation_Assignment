e08a55a6396d9622f426aaead8ec2cd7aa7d34cc
BOYA SAITEJA <boyasaiteja007@gmail.com>
2021-08-20 20:12:23 +0530

    first commit

---
+++ src/Pages/home.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react';
+import HomeCard from '../components/homeCard';
+import NotBookedScreen from './notBookedScreen';
+
+function Home(props){
+    
+    
+    const [isRoombooked, setisRoombooked]=useState(false);
+    
+  return(
+    <>
+      {
+        isRoombooked ? (<HomeCard />) : (<NotBookedScreen />)
+      }
+
+    </>
+  );
+
+}
+export default Home;
